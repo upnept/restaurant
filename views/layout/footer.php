150db458c9f2ef888869ca4675e25e1fcb61e502
upnept <chris.mejia@southeasttech.edu>
2026-07-19 12:55:13 -0500

    layout changes

---
+++ views/layout/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> Resteraunt, Co.
+    </p>
+</footer>
+</body>
+</html>
---
+++ views/layout/header.php
@@ -0,0 +1,28 @@
+<?php if (isset($_SESSION['message'])) : ?>
+    <div class="message">
+        <?= htmlspecialchars($_SESSION['message']) ?>
+    </div>
+    <?php unset($_SESSION['message']); ?>
+<?php endif; ?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Resteraunt</title>
+        <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
+
+    <body>
+        <header>
+            <h1>Resteraunt</h1>
+        </header>
+
+        <header>
+            <nav>
+                <p><a href=".?action=menu">Menu</a></p>
+                <p><a href=".?action=cart">Cart</a></p>
+                <p><a href=".?action=login">Login</a></p>
+                <p><a href=".?action=register">Register</a></p>
+            </nav>
+        </header>
