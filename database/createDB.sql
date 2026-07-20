7d6768375abc7bf404a4ace6469e34eb3964c68a
upnept <chris.mejia@southeasttech.edu>
2026-07-20 13:51:38 -0500

    db pass/admin

--- database/createDB.sql
+++ database/createDB.sql
@@ -5,8 +5,10 @@ CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     phone VARCHAR(20),
-    address VARCHAR(255)
+    address VARCHAR(255),
+    is_admin TINYINT(1) NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS menu_items (
