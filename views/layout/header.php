19909f459ba26873477c65feb6fbdd6cefba1c69
upnept <chris.mejia@southeasttech.edu>
2026-07-20 16:02:06 -0500

    functionality for header

--- views/layout/header.php
+++ views/layout/header.php
@@ -10,19 +10,26 @@
 <html>
     <head>
         <title>Restaurant</title>
-        <link rel="stylesheet" type="text/css" href="main.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
 
     <body>
         <header>
             <h1>Restaurant</h1>
+            <p><?php echo $welcomeMessage; ?></p>
         </header>
 
         <header>
             <nav>
                 <p><a href=".?action=menu">Menu</a></p>
                 <p><a href=".?action=cart">Cart</a></p>
-                <p><a href=".?action=login">Login</a></p>
-                <p><a href=".?action=register">Register</a></p>
+                <?php if (isset($_SESSION['user_id'])) : ?>
+                    <p><a href=".?action=order_history">My Orders</a></p>
+                    <p>Hi, <?= htmlspecialchars($_SESSION['user_name']) ?></p>
+                    <p><a href=".?action=logout">Logout</a></p>
+                <?php else : ?>
+                    <p><a href=".?action=login">Login</a></p>
+                    <p><a href=".?action=register">Register</a></p>
+                <?php endif; ?>
             </nav>
         </header>
