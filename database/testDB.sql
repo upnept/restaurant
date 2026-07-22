5ad943d957c35acce95d6e4a5256e6875ac44c63
upnept <chris.mejia@southeasttech.edu>
2026-07-21 20:17:06 -0500

    admin creds

--- database/testDB.sql
+++ database/testDB.sql
@@ -1,7 +1,7 @@
 USE restaurant;
 
 INSERT IGNORE INTO users (name, email, password, phone, address, is_admin)
-VALUES ('admin', 'admin@gmail.com', '$2y$10$Q7SnEecfURHM.eafcUkfBOkrJK/FDhDG3Y1HMndTJyC0ynPIWUbPi', '000-000-0000', '000 Str', 1);
+VALUES ('admin', 'admin@gmail.com', '$2y$10$38EbrA8.v0VQ/AAMHPmozOXp9J8u1ExrIQWENsZ4WeqXgrtPECnqa', '000-000-0000', '6969 Str', 1);
 
 INSERT INTO menu_items (name, description, price, category, image_url) VALUES
 ('CheeseBurger', 'A classic cheeseburger', 9.99, 'Food', ''),
