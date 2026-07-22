6928bfc00f5c9bd622003d5a3d2f623188d3e023
upnept <chris.mejia@southeasttech.edu>
2026-07-21 19:09:42 -0500

    added pass

--- database/testDB.sql
+++ database/testDB.sql
@@ -1,9 +1,9 @@
 USE restaurant;
 
 INSERT IGNORE INTO users (name, email, password, phone, address, is_admin)
-VALUES ('Admin', 'admin@gmail.com', '', '000-000-0000', '000 Str', 1);
+VALUES ('admin', 'admin@gmail.com', '$2y$10$Q7SnEecfURHM.eafcUkfBOkrJK/FDhDG3Y1HMndTJyC0ynPIWUbPi', '000-000-0000', '000 Str', 1);
 
 INSERT INTO menu_items (name, description, price, category, image_url) VALUES
 ('CheeseBurger', 'A classic cheeseburger', 9.99, 'Food', ''),
 ('Fries', 'French fries', 3.99, 'Food', ''),
-('Soda', 'Any soda', 2.99, 'Drink', ''),
+('Soda', 'Any soda', 2.99, 'Drink', '');
